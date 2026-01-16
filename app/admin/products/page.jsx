356063ff44f647b6a3d20816e64bd1573caad15c
evangtsystems <evangelos.lampos@gtsystems.gr>
2026-01-16 11:06:01 +0200

    /

--- app/admin/products/page.jsx
+++ app/admin/products/page.jsx
@@ -682,20 +682,7 @@ useEffect(() => {
     🗂 Κατηγορίες
   </button>
 
-        <button
-  onClick={() => (window.location.href = "/admin/trash")}
-  style={{
-    background: "#1f3b2e",
-    color: "#fff",
-    border: "none",
-    borderRadius: 6,
-    padding: "8px 14px",
-    fontWeight: "bold",
-    cursor: "pointer",
-  }}
->
-  🗑 Κάδος
-</button>
+       
 
 
         <button
